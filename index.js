12270fae36a035ee7c9390a68f077324973e9001
Felix Kipruto <kimosopfelix@gmail.com>
2022-12-06 18:51:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+// Add your code her
+
+const submitData = {
+    userName : "felix",
+    email : "fkc@gmail.com",
+};
+
+fetch("  http://localhost:3000/users", {
+
+method: "POST",
+headers:{
+    'Content-Type': 'application/json'
+    
+  },
+body:JSON.stringify(submitData)
+
+})
+
+.then(res => res.json())
+
+.then(data => console.log(data))
